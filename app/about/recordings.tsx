dc6e774fd04f48776962fdfcae0fb4884824b270
Daniel Molloy <danielmolloy_6@icloud.com>
2024-05-10 22:22:44 +0100

    removed react-scroll

--- app/about/recordings.tsx
+++ app/about/recordings.tsx
@@ -2,7 +2,6 @@ import { Recording } from "@/lib/soundtracks"
 import Image from "next/image";
 import { useState } from "react";
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
-import { Element, Button, animateScroll, Link } from "react-scroll";
 
 
 export type RecordingsProps = {
